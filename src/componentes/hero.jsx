fe09287aa024d1ab547378894a2a92b7f2df2411
Jespindola15 <Jespindola@frsn.utn.edu.ar>
2026-04-28 18:44:26 -0300

    video=?

--- src/componentes/hero.jsx
+++ src/componentes/hero.jsx
@@ -1,10 +1,11 @@
 import './hero.css'
+import heroVideo from '/hero-video.mp4?url' // Usamos ?url para que Vite nos dé la ruta correcta
 
 export default function Hero() {
   return (
     <section className="hero">
       <video autoPlay loop muted playsInline className="hero-video" key="hero-video">
-        <source src="./hero-video.mp4" type="video/mp4" />
+        <source src={heroVideo} type="video/mp4" />
         Tu navegador no soporta el elemento de video.
       </video>
       <div className="hero-overlay"></div>
@@ -14,6 +15,6 @@ export default function Hero() {
         <a href="#carta" className='hero-button'>Ver Menú</a>
       </div>
     </section>
-    
   )
 }
+
