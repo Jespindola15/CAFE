cfe4936342af4cb4b2efd577c305aa2ace4dde2a
Jespindola15 <Jespindola@frsn.utn.edu.ar>
2026-04-28 18:46:45 -0300

    video final

--- src/componentes/hero.jsx
+++ src/componentes/hero.jsx
@@ -1,20 +1,19 @@
 import './hero.css'
-import heroVideo from '/hero-video.mp4?url' // Usamos ?url para que Vite nos dé la ruta correcta
 
 export default function Hero() {
   return (
     <section className="hero">
-      <video autoPlay loop muted playsInline className="hero-video" key="hero-video">
-        <source src={heroVideo} type="video/mp4" />
-        Tu navegador no soporta el elemento de video.
+      <video autoPlay loop muted playsInline className="hero-video">
+        <source src="/hero-video.mp4" type="video/mp4" />
       </video>
       <div className="hero-overlay"></div>
       
       <div className="hero-content">
-        <img src="/logo.png" alt="Café de la Plaza Logo" className="hero-logo" />
-        <a href="#carta" className='hero-button'>Ver Menú</a>
+        <img src="/logo.png" alt="Logo" className="hero-logo" />
+        <a href="#carta" className="hero-button">Ver Menú</a>
       </div>
     </section>
   )
 }
+
 
