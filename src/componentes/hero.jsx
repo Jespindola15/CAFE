21f25858700c6cdba16a16ce91a1e25bd08f974e
Jespindola15 <Jespindola@frsn.utn.edu.ar>
2026-04-28 18:41:23 -0300

    c

--- src/componentes/hero.jsx
+++ src/componentes/hero.jsx
@@ -3,15 +3,13 @@ import './hero.css'
 export default function Hero() {
   return (
     <section className="hero">
+      <video autoPlay loop muted playsInline className="hero-video">
+        <source src="/hero-video.mp4" type="video/mp4" />
+      </video>
+      <div className="hero-overlay"></div>
       
       <div className="hero-content">
-        <div className="hero-overlay">
-        <video autoPlay loop muted playsInline className="hero-video">
-        <source src="/hero-video.mp4" type="video/mp4" />
-        </video>
-      </div>
-        <h1>Café</h1>
-        <h3>De la Plaza Mitre</h3>
+        <img src="/logo.png" alt="Café de la Plaza Logo" className="hero-logo" />
         <a href="#carta" className='hero-button'>Ver Menú</a>
       </div>
     </section>
